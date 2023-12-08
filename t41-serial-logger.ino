9fe9e1d8b914f62da92f57d08b451a911c9e2af0
njl7502l <g.hana1211.tako@gmail.com>
2023-12-08 22:55:18 +0900

    仕上がってきたんじゃないの?

--- t41-serial-logger.ino
+++ t41-serial-logger.ino
@@ -1,38 +1,56 @@
 #include "SdFat.h"
- 
+#include "USBHost_t36.h"
+#define USBBAUD 115200
+
 // Use Teensy SDIO
 #define SD_CONFIG  SdioConfig(FIFO_SDIO)
  
+uint32_t baud = USBBAUD;
+uint32_t format = USBHOST_SERIAL_8N1;
+USBHost myusb;
+USBSerial userial(myusb);
+
 SdFs sd;
 FsFile file;
 
-void writeSD(String message){
+void writeSD(char* message, uint16_t n ){
+    digitalWrite(13, HIGH);
     file = sd.open("/logs.txt", FILE_WRITE);
     if(!file){
-        Serial.println("Failed to open file for writing");
-        return;
+      Serial.println("Failed to open file for writing");
+      return;
     }
-    file.println(message);
+    file.seekEnd();
+    file.write(message, n);
     file.close();
+    digitalWrite(13,LOW);
 }
 
 void setup() {
-    Serial.begin(921600);
-
+    pinMode(13, OUTPUT);
     delay(500);
+    Serial.begin(115200);
+
     if(!sd.begin(SD_CONFIG)){
-        Serial.println("Card Mount Failed");
-        return;
+      Serial.println("Card Mount Failed");
+      return;
     }
-    String divider = "###################################################################";
-    writeSD(divider);
+    char divider[100] = "################################################################################\n";
+
+    writeSD(divider, sizeof(divider));
+
+    myusb.begin();
+    userial.begin(baud);
 }
 
-String incomingByte;
+// String incomingByte;
+char buffer[512];
+uint16_t n, rd;
 void loop() {
-  if (Serial.available() > 0) {
-    incomingByte = Serial.readStringUntil('\n');
-    writeSD(incomingByte);
+  rd = userial.available();
+  if (rd > 0) {
+    n = userial.readBytes((char *)buffer, rd);
+    writeSD(buffer, n);
   }
   yield();
 }
