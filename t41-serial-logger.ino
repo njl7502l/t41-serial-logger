91e0c114a52c0bade511622098100e88bd559fa1
njl7502l <g.hana1211.tako@gmail.com>
2023-12-12 15:17:53 +0900

    t41とのみ疎通ができない問題を解決

--- t41-serial-logger.ino
+++ t41-serial-logger.ino
@@ -1,41 +1,46 @@
 #include "SdFat.h"
 #include "USBHost_t36.h"
-#define USBBAUD 115200
+#define USBBAUD 921600
 
 // Use Teensy SDIO
-#define SD_CONFIG  SdioConfig(FIFO_SDIO)
- 
+#define SD_CONFIG SdioConfig(FIFO_SDIO)
+
 uint32_t baud = USBBAUD;
 uint32_t format = USBHOST_SERIAL_8N1;
 USBHost myusb;
-USBSerial userial(myusb);
+// USBSerial userial(myusb);
+USBSerial_BigBuffer userial(myusb, 1);
 
 SdFs sd;
 FsFile file;
 
-void writeSD(char* message, uint16_t n ){
+/// @brief SDカードに書き込み
+/// @param message 配列として定義したバイト列
+/// @param n 配列の長さ
+void writeSD(char *buf, uint16_t len) {
     digitalWrite(13, HIGH);
     file = sd.open("/logs.txt", FILE_WRITE);
-    if(!file){
-      Serial.println("Failed to open file for writing");
-      return;
+    if (!file) {
+        Serial.println("Failed to open file for writing");
+        return;
     }
     file.seekEnd();
-    file.write(message, n);
+    file.write(buf, len);
     file.close();
-    digitalWrite(13,LOW);
+    digitalWrite(13, LOW);
 }
 
 void setup() {
     pinMode(13, OUTPUT);
     delay(500);
-    Serial.begin(115200);
+    Serial.begin(baud);
 
-    if(!sd.begin(SD_CONFIG)){
-      Serial.println("Card Mount Failed");
-      return;
+    if (!sd.begin(SD_CONFIG)) {
+        Serial.println("Card Mount Failed");
+        return;
     }
-    char divider[100] = "################################################################################\n";
+    char divider[100] = "########################################################"
+                        "########################\n";
 
     writeSD(divider, sizeof(divider));
 
@@ -44,13 +49,13 @@ void setup() {
 }
 
 // String incomingByte;
-char buffer[512];
+char buffer[4096];
 uint16_t n, rd;
 void loop() {
-  rd = userial.available();
-  if (rd > 0) {
-    n = userial.readBytes((char *)buffer, rd);
-    writeSD(buffer, n);
-  }
-  yield();
+    rd = userial.available();
+    if (rd > 0) {
+        n = userial.readBytes((char *)buffer, rd);
+        writeSD(buffer, n);
+    }
+    yield();
 }
