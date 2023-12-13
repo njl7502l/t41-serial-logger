ff5c6d3d4a63ca19ee7a1061b8b7d8e9a970eccc
njl7502l <g.hana1211.tako@gmail.com>
2023-12-13 13:56:33 +0900

    タイムスタンプ追加

--- t41-serial-logger.ino
+++ t41-serial-logger.ino
@@ -1,5 +1,7 @@
 #include "SdFat.h"
 #include "USBHost_t36.h"
+#include <TimeLib.h>
+
 #define USBBAUD 921600
 
 // Use Teensy SDIO
@@ -16,17 +18,38 @@ FsFile file;
 
 int hoge;
 
+String fileName;
+
+time_t getTeensy3Time() {
+    return Teensy3Clock.get();
+}
+
+String filDigit(int digits) {
+    if (digits < 10) {
+        return '0' + String(digits);
+    } else {
+        return String(digits);
+    }
+}
+String getDatetimeString() {
+    String date = filDigit(year()) + '-' + filDigit(month()) + '-' + filDigit(day());
+    String time = filDigit(hour()) + '-' + filDigit(minute()) + '-' + filDigit(second());
+    return date + "_" + time;
+}
+
 /// @brief SDカードに書き込み
 /// @param message 配列として定義したバイト列
 /// @param n 配列の長さ
 void writeSD(char *buf, uint16_t len) {
     digitalWrite(13, HIGH);
-    file = sd.open("/logs.txt", FILE_WRITE);
+    file = sd.open(fileName.c_str(), FILE_WRITE);
     if (!file) {
         Serial.println("Failed to open file for writing");
         return;
     }
     file.seekEnd();
+    file.print(getDatetimeString());
+    file.print(" >\t");
     file.write(buf, len);
     file.close();
     digitalWrite(13, LOW);
@@ -34,15 +57,23 @@ void writeSD(char *buf, uint16_t len) {
 
 void setup() {
     pinMode(13, OUTPUT);
-    delay(500);
     Serial.begin(baud);
+
+    setSyncProvider(getTeensy3Time);
+
+    if (timeStatus() != timeSet) {
+        fileName = "/logs.txt";
+    } else {
+        fileName = "/" + getDatetimeString() + ".txt";
+    }
 
     if (!sd.begin(SD_CONFIG)) {
         Serial.println("Card Mount Failed");
         return;
     }
-    char divider[100] = "########################################################"
-                        "########################\n";
+
+    file.open(fileName.c_str(), O_WRONLY | O_CREAT);
+    char divider[] = "########################################################\n";
 
     writeSD(divider, sizeof(divider));
 
